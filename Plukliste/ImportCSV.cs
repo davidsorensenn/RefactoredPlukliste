14aed4fca011008fe60982ddedeb95c4d9800a25
Kenni <kenn7575@gmail.com>
2023-02-17 11:04:05 +0100

    5.1.1

--- Plukliste/ImportCSV.cs
+++ Plukliste/ImportCSV.cs
@@ -7,18 +7,18 @@ namespace Plukliste
     {
         public Pluklist Read(string path)
         {
-        Pluklist pluklist = new();
+            Pluklist pluklist = new();
             pluklist.Adresse = "Pickup";
-           
-           string name = path.Substring(path.IndexOf("_") + 1);
-           name = name.Substring(0, name.IndexOf("."));
-           name = name.Replace("_", " ");
+
+            string name = path.Substring(path.IndexOf("_") + 1);
+            name = name.Substring(0, name.IndexOf("."));
+            name = name.Replace("_", " ");
             pluklist.Name = name;
-           pluklist.Forsendelse = "Pickup";    
+            pluklist.Forsendelse = "Pickup";
 
             List<string> data = File.ReadAllLines(path).ToList();
             data.RemoveAt(0);
-            foreach(var item in data)
+            foreach (var item in data)
             {
                 Item line = new Item();
                 var values = item.Split(';');
